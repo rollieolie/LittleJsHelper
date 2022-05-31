627419f55f3306fadb175b960c14d0b6d00f03c8
rollieolie <56703680+rollieolie@users.noreply.github.com>
2022-05-31 14:18:08 -0500

    Create CopyFilesUserbin.sh

---
+++ CopyFilesUserbin.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp GetUrl.py /usr/bin/GetUrl;
+chmod +x /usr/bin/GetUrl;
+cp beauty_js.py /usr/bin/beauty_js;
+chmod +x /usr/bin/beauty_js;
+cp LittleJsHelper.sh /usr/bin/LittleJsHelper;
+chmod +x /usr/bin/LittleJsHelper;
+
+echo "Programs are copied to your /usr/bin"
